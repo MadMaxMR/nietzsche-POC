f5dd3273e4b2b4814700103352250d64d44fb565
madmax <madmaxmolina14@gmail.com>
2023-03-30 00:38:02 -0500

    ""

--- script2.js
+++ script2.js
@@ -14,7 +14,18 @@ var svg = d3
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-//bar chart
+
+var svg2 = d3
+    .select(".chart2")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .style("border", "1px solid red")
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+//bar chart + INFO
 const svg1 = d3
     .select(".chart1")
     .append("svg")
@@ -26,15 +37,14 @@ const svg1 = d3
     .append("g")
     .attr("class","graph3");
 
-var svg2 = d3
-    .select(".chart2")
+var linegraph = d3
+    .select(".PeopleLineGraph")
     .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr("width", width + margin.left + margin.right+80)
+    .attr("height", height + margin.top + margin.bottom-200)
     .style("border", "1px solid red")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
 
 
 var keys_resp=  ["philosophy", "people", "religion","general"];
@@ -238,6 +248,7 @@ d3.json("data.json").then(function(data){
         .call(xAxis);
 
 BarChart();
+linearGraph();
 });
 
 
@@ -366,7 +377,6 @@ function BarChart(){
     ].map(e => e.join(","))
     .join("\n");
     
-
     var data = d3.csvParse(newCSV)
     
     var subgroups = data.columns.slice(1)
@@ -451,7 +461,7 @@ function SelectChart(key) {
 
     d3.selectAll("."+key)
           .style("opacity", 1)
-          .attr("fill", colorScaleSub[key] )
+          .attr("fill", colorScaleSub[key]);
 }
 
 var totalTabs = $('.nav-tabs li').length;
@@ -486,9 +496,235 @@ $('#prevbtn').click(function(e){
             .html("<p>YEAR:<b>"+dataFragment[count].year+"</b> | GROUP:<b>"+dataFragment[count].group+"</b> | SEASON:<b>"+dataFragment[count].season +"</b></p>")
             .style("color","red");
             
-    d3.selectAll(".fragmentText").html("")
+    d3.selectAll(".fragmentText").html("");
 
     dataFragment[count].fragments.forEach(function(fragments){
         d3.selectAll(".fragmentText").append("div").html(fragments)
     });
 });
+
+
+//CREACION DEL GRAFICO LINEAL
+function linearGraph() {
+    const peopleCSV = [
+        [
+            "year",
+            "pWagn","pSchop","pKant"
+        ],
+        ...dataRex.map(data =>[
+            data.year.getFullYear(),
+            data.pWagn,data.pSchop,data.pKant
+        ])
+    ].map(e => e.join(","))
+    .join("\n");
+
+    const religionCSV = [
+        [
+            "year",
+            "pHindu","pVedanta","pBudd","pSchleier"
+        ],
+        ...dataRex.map(data =>[
+            data.year.getFullYear(),
+            data.pHindu,data.pVedanta,data.pBudd,data.pSchleier
+        ])
+    ].map(e => e.join(","))
+    .join("\n");
+
+    const philosophyCSV = [
+        [
+            "year",
+            "pWille","pDing","pWahr","pLeiden","pTragische"
+        ],
+        ...dataRex.map(data =>[
+            data.year.getFullYear(),
+            data.pWille,data.pDing,data.pWahr,data.pLeiden,data.pTragische
+        ])
+    ].map(e => e.join(","))
+    .join("\n");
+
+
+    const germanyCSV = [
+        [
+            "year",
+            "pDeutsch","pMusik"
+        ],
+        ...dataRex.map(data =>[
+            data.year.getFullYear(),
+            data.pDeutsch,data.pMusik
+        ])
+    ].map(e => e.join(","))
+    .join("\n");
+
+    
+    //Data de people
+    var dataPeople = d3.csvParse(peopleCSV)
+    var keysPeople = dataPeople.columns.slice(1)
+
+    console.log("data peolple" , dataPeople)
+
+
+    //Data de Religion
+    var dataReligion = d3.csvParse(religionCSV)
+    var keysReligion = dataReligion.columns.slice(1)
+
+    //Data de Philosophy
+    var dataPhilosophy = d3.csvParse(philosophyCSV)
+    var keysPhilosophy = dataPhilosophy.columns.slice(1)
+
+    //Data de Germany
+    var dataGermany = d3.csvParse(germanyCSV)
+    var keysGermany = dataGermany.columns.slice(1)
+
+
+
+    var datastackePeople = d3.stack()
+        .keys(keysPeople)(dataPeople);
+    
+    var x = d3.scaleLinear()
+        .domain(d3.extent(dataPeople, function(d) { return d.year; }))
+        .range([ 0, width+100]);
+
+    var y = d3.scaleLinear()
+        .domain([0, 100])
+        .range([ height-200, 0 ]);
+    
+    var area = d3.area()
+        .x(function(d) { return x(d.data.year); })
+        .y0(function(d) { return y(d[0]); })
+        .y1(function(d) { return y(d[1]); })
+        .curve(d3.curveCatmullRom.alpha(0.1));
+        
+
+    var valC = false
+
+    //CREACION DEL POPUP
+    var Tooltip = d3.select(".PeopleLineGraph")
+                    .append("div")
+                    .style("opacity", 0)
+                    .attr("class", "tooltip")
+                    .style("background-color", "#222222")
+                    .style("border", "solid")
+                    .style("border-width", "2px")
+                    .style("border-radius", "5px")
+                    .style("border-color", "#222222")
+                    .style("padding", "5px")
+                    .style("color", "white")
+
+    var textClick = function(d,event){
+        valC = true
+        //alert(valC)
+        if(valC==true){
+            var key = d.target.__data__
+            console.log(key)
+
+            d3.selectAll(".linealG")
+            .style("opacity",0.5);
+            
+            d3.selectAll(".linalT")
+            .style("fill", "white");
+            
+            d3.select(".lnl"+key)
+            .style("opacity",1)
+
+            d3.select(".lnltxt"+key)
+            .style("fill", eval("colorScaleSub."+key));
+            
+            //CREACIÓN DE LOS CIRCULOS EN LAS LINEAS
+            linegraph.selectAll("myCircles")
+                .data(dataPeople)
+                .enter()
+                .append("circle")
+                .attr("fill", "black")
+                .attr("stroke", "none")
+                .attr("cx", function(d) { return x(d.year) })
+                .attr("r", 5)
+                .attr("cy", function(d ,i ) {
+                    const keys = Object.keys(d)
+                    const index = keys.indexOf(key)
+                    var stackedValue = 0
+                    const values = Object.values(d)
+                    for (let i = 0; i < index; i++) {
+                        stackedValue+=parseInt(values[i+1])
+                    }
+                    console.log("Value de D", d);
+                    console.log("INDICE:" , index, "STACKE VALUE" ,stackedValue)
+                    //return y(eval("d."+key))
+                    return  y(stackedValue)
+                })
+                .on("click", function(d) {
+                    popData = d.target.__data__
+                    Tooltip.transition()
+                        .duration(200)
+                        .style("opacity", .9)
+                    d3.select(this)
+                        .style("stroke", "black")
+                        .style("opacity", 1);
+                     
+                    Tooltip
+                        .html("The exact value of<br>this cell is: " + popData.year)
+                        .style("left", (d.pageX-50) + "px")
+                        .style("top", (d.pageY-60) + "px")
+                    //alert("Año: "+ popData.year+" "+key+": "+ eval("popData."+key))
+                  })
+                /*.on("mouseleave",function(d){
+                    Tooltip
+                        .style("opacity", 0)
+                    d3.select(this)
+                    .style("stroke", "none")
+                    .style("opacity", 0.8)
+                })*/
+        }
+        if(valC==false){
+            d3.selectAll(".linalT")
+            .style("fill", "white");
+
+            d3.selectAll(".linealG")
+                .style("opacity",1);
+        }
+
+    }
+
+    linegraph.append("g")
+        .selectAll("g")
+        .data(datastackePeople)
+        .enter()
+        .append("path")
+        .attr("class", function(d) { return "linealG" + " lnl"+d.key })
+        .style("fill", colorScale.people)
+        .attr("d", area);
+
+    
+
+    linegraph.selectAll("mylabels").append("g")
+        .data(keysPeople)
+        .enter()
+        .append("text")
+            .attr("x", function(d,i){
+                if(i>0){
+                    return 0 + i*(200)+((i-1)*(140))
+                }
+                return  0
+                })
+            .attr("y",50)
+            .text(function(d){ 
+                if(d=="pWagn"){
+                    return "Wagner"
+                }
+                if(d=="pSchop"){
+                    return "Schopenhauer"
+                }
+                if(d=="pKant"){
+                    return "Kant"
+                }
+            })
+            .attr("text-anchor", "left")
+            .attr("class", function(d){return "linalT lnltxt"+d})
+            .style("alignment-baseline", "middle")
+            .style("font-size","56px")
+            .style("fill", "white")
+            .style("font-family", "PlayfairDisplay-Regular")
+            .style("cursor","pointer")
+            .on("click",function(d){textClick(d,d3.event);});
+     
+}
+
